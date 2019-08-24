653a131b29c3ccef92adcc5da6b1b449a01c9879
Noble Varghese <varghese.noble96@gmail.com>
2019-08-25 02:41:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,31 +9,38 @@ import { Link } from 'react-router-dom';
 
 
 class App extends Component {
+
+
+  
+    hideToggle() {
+        var selectorId = document.querySelector('.mdl-layout');
+        selectorId.MaterialLayout.toggleDrawer();
+    }
+
   render() {
     return (
         
         <div className="demo-big-content" >
             <Layout>
-                <Header className="header-color" title="">
+                <Header className="header-color" style={{color:"black"}}>
                     <Navigation>
                       <Link to="/home">Home</Link>
                       <Link to="/resume">Resume</Link>
-                      <Link to="/aboutme">About Me</Link>
+                      {/* <Link to="/aboutme">About Me</Link> */}
                       <Link to="/projects">Projects</Link>
                       <Link to="/contact">Contact</Link>
                     </Navigation>
                 </Header>
-                <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/home">My portfolio</Link>}>
+                <Drawer>
                     <Navigation>
-                      <Link to="/home">Home</Link>
-                      <Link to="/resume">Resume</Link>
-                      <Link to="/aboutme">About Me</Link>
-                      <Link to="/projects">Projects</Link>
-                      <Link to="/contact">Contact</Link>
+                      <Link to="/home" onClick={()=> this.hideToggle()}>Home</Link>
+                      <Link to="/resume" onClick={()=> this.hideToggle()}>Resume</Link>
+                      {/* <Link to="/aboutme" onClick={()=> this.hideToggle()}>About Me</Link> */}
+                      <Link to="/projects" onClick={()=> this.hideToggle()}>Projects</Link>
+                      <Link to="/contact" onClick={()=> this.hideToggle()}>Contact</Link>
                     </Navigation>
                 </Drawer>
                 <Content>
-                    <div className="page-content" />
                     <Main/>
                 </Content>
             </Layout>
